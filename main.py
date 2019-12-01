a1cb2c9fa6c9458743f3aba19585cf556ae75893
Miles <miles-moran@hotmail.com>
2019-12-01 06:30:04 -0600

    test

--- main.py
+++ main.py
@@ -14,38 +14,40 @@ def test():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver.get("https://listingcenter.nasdaq.com/noncompliantcompanylist.aspx")
-    expand = driver.find_element_by_class_name("rgExpand")
+    browser.get("https://listingcenter.nasdaq.com/noncompliantcompanylist.aspx")
+    expand = browser.find_element_by_class_name("rgExpand")
     expand.click()
     time.sleep(1)
-    headers = driver.find_elements_by_xpath('//*[contains(@class, "rgGroupHeader")]//p')
-    details = driver.find_elements_by_xpath('//*[contains(@title, "Quote")]/parent::*/following-sibling::td')
     data = []
-
-    for x in headers:
-        company = {
-            "name": "",
-            "ticker": "",
-            "deficiency": "",
-            "market": "",
-            "date": ""
-        }
-        company["name"] = x.text
-        data.append(company)
-    i = 1
-    counter = 0
-    for x in details:
-        if (i == 1):
-            data[counter]["ticker"] = x.text
-        elif (i == 2):
-            data[counter]["deficiency"] = x.text
-        elif (i == 3):
-            data[counter]["market"] = x.text
-        elif (i == 4):
-            data[counter]["date"] = x.text
+    rows = browser.find_elements_by_xpath('//table[contains(@class, "rgMasterTable")]/tbody/tr')
+    header = ""
+    for row in rows:
+        className = row.get_attribute('class')
+        if className == "rgGroupHeader":
+            header = row.find_element_by_xpath(".//td[last()]/p")
+        else:
+            company = {
+                "name": header.text,
+                "tickers": [],
+                "deficiency": "",
+                "market": "",
+                "date": ""
+            }
+            tickerContainer = row.find_element_by_xpath('.//*[contains(@class, "LeftPadding")]')
+            tickers = tickerContainer.find_elements_by_xpath(('.//a'))
+            for t in tickers:
+                company["tickers"].append(t.text)
+            categories = tickerContainer.find_elements_by_xpath('.//following-sibling::td')
             i = 0
-            counter += 1
-        i += 1
+            for category in categories: 
+                if i == 0: 
+                    company["deficiency"] = category.text
+                elif i == 1:
+                    company["market"] = category.text
+                elif i == 2: 
+                    company["date"] = category.text
+                i += 1
+            data.append(company)
     return jsonify(data)
 
 if __name__ == '__main__':
