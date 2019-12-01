00dae52b30c9d83098608a8ce0c73607f452d33c
Miles <miles-moran@hotmail.com>
2019-12-01 03:20:20 -0600

    test

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def test():
-    print("test")
-    return 'Hello World'
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver.get("https://listingcenter.nasdaq.com/noncompliantcompanylist.aspx")
+    expand = driver.find_element_by_class_name("rgExpand")
+    return expand.toString()
 
 if __name__ == '__main__':
     app.run()
-
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--disable-dev-shm-usage")
-# chrome_options.add_argument("--no-sandbox")
-# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
