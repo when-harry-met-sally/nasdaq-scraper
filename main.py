f75b2eb64b4fed4c251fe467fcd6f6218d00bba2
Miles <miles-moran@hotmail.com>
2019-12-01 03:44:30 -0600

    test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from flask import Flask
+from flask import jsonify
+import time
 import os
 
 app = Flask(__name__)
@@ -14,7 +16,38 @@ def test():
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get("https://listingcenter.nasdaq.com/noncompliantcompanylist.aspx")
     expand = driver.find_element_by_class_name("rgExpand")
-    return expand
+    expand.click()
+    time.sleep(1)
+    headers = driver.find_elements_by_xpath('//*[contains(@class, "rgGroupHeader")]//p')
+    details = driver.find_elements_by_xpath('//*[contains(@title, "Quote")]/parent::*/following-sibling::td')
+    data = []
+
+    for x in headers:
+        company = {
+            "name": "",
+            "ticker": "",
+            "deficiency": "",
+            "market": "",
+            "date": ""
+        }
+        company["name"] = x.text
+        data.append(company)
+    i = 1
+    counter = 0
+    for x in details:
+        print(i)
+        if (i == 1):
+            data[counter]["ticker"] = x.text
+        elif (i == 2):
+            data[counter]["deficiency"] = x.text
+        elif (i == 3):
+            data[counter]["market"] = x.text
+        elif (i == 4):
+            data[counter]["date"] = x.text
+            i = 0
+            counter += 1
+        i += 1
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run()
