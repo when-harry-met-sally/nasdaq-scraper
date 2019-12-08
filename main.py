f4ca287fc1ec7228939957af93428ab7979100c6
Miles <miles-moran@hotmail.com>
2019-12-08 05:45:41 -0600

    added sherdog scraper while manic programming

--- main.py
+++ main.py
@@ -2,10 +2,71 @@ from selenium import webdriver
 from flask import Flask
 from flask import jsonify
 from lxml import html
+import requests
 import time
 import os
 
 app = Flask(__name__)
+def makeRequestAndGetTree(URL):
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+        'Accept-Language': 'en-gb',
+        'Accept-Encoding': 'br, gzip, deflate',
+        'Accept': 'test/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    }
+    try:
+        r = requests.get(URL, headers=headers, timeout=5)
+        tree = html.fromstring(r.content)
+        return tree
+    except Exception as e:
+        print(e)
+        return None
+
+def scrapeCalendar(URL):
+    tree = makeRequestAndGetTree(URL)
+    if tree is None:
+        return None
+    eventContainers = tree.xpath("//tr[@item-type='http://schema.org/Event']")
+    print(len(eventContainers))
+    print(tree)
+
+@app.route("/sherdog/events")
+def scrape():
+    URL = 'http://www.sherdog.com/events/upcoming'
+    events = scrapeCalendar(URL)
+    events = jsonify(events)
+    return events
+    
+def scrapeCalendar(URL):
+    tree = makeRequestAndGetTree(URL)
+    if tree is None:
+        return None
+    events = []
+    containers = tree.xpath("//tr[@itemtype='http://schema.org/Event']")
+    for container in containers:
+        date = container.xpath(".//meta[@itemprop='startDate']")
+        date = date[0].attrib.get("content") if len(date) is not 0 else None
+        name = container.xpath(".//meta[@itemprop='name']")
+        name = name[0].attrib.get("content") if len(name) is not 0 else None
+        org = None
+        if (name is not None):
+            name = name.split("-")
+            title = name[1].strip()
+            org = name[0].strip()
+        location = container.xpath(".//span[@itemprop='location']")
+        location = location[0].text_content() if len(location) is not 0 else None
+        link = container.xpath(".//a[@itemprop='url']")
+        link = 'https://www.sherdog.com/' + link[0].attrib.get("href") if len(link) is not 0 else None
+        event = {
+            "date": date,
+            "name": title,
+            "location": location,
+            "link": link,
+            "org": org
+        }
+        print(event)
+        events.append(event)
+    return events
 
 @app.route("/")
 def test():
