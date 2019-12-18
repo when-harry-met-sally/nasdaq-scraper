6570892424002bfb1c594c47810f558006aa559b
Miles <miles-moran@hotmail.com>
2019-12-17 18:02:41 -0600

    test

--- main.py
+++ main.py
@@ -60,8 +60,8 @@ def setFavorites():
     token = 'api1575404422oY4rNBhdTW9JjgfyulEi78894'
     calendar = '1576110713243225' 
     for row in req_data:
-        i = row[0]
-        favorite = row[1]
+        i = str(row[0])
+        favorite = str(row[1])
         URL = 'https://www.addevent.com/api/v1/me/calendars/events/save/?token=' + token +'&calendar_id=' + calendar +'&event_id='+ i + '&organizer_email=' + favorite
         r = requests.get(URL)
     return jsonify(req_data)
